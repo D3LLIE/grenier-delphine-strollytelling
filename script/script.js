455381ddd97d319b9b90c28d9ffdd75cbe1e7c30
Delphine <112188978+DelphineG-Projets@users.noreply.github.com>
2024-04-15 17:56:04 -0400

    Ajout images et animations js

--- script/script.js
+++ script/script.js
@@ -12,3 +12,13 @@ window.addEventListener('scroll', function() {
         body.classList.remove('is-scrolling');
        }, 100); 
   });
+
+  gsap
+  .timeline()
+  .to(".para00",{x:"-40%",duration:18,ease:'none'},'<')
+  .to(".para04",{y:"2%", yoyo: true, repeat: 40, duration:0.5 ,ease:'none'},'<')
+  .to(".para01",{x:"-40%",duration:17,ease:'none'},'<')
+  .to(".para02",{x:"-40%",duration:16,ease:'none'},'<')
+  .to(".para03",{x:"-40%",duration:14,ease:'none'},'<')
+  .to(".para05",{x:"-40%",duration:12,ease:'none'},'<')
+  .to(".para04",{x:"200%",duration:10 ,ease:'none'}, 12)

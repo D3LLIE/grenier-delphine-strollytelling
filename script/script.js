5cdd983c302ebf16cf6a783f725ee9d13beb2567
Delphine <delphinegren@gmail.com>
2024-04-28 00:12:21 -0400

    Ajout animation chapitre 5

--- script/script.js
+++ script/script.js
@@ -97,7 +97,7 @@ let chapitreTrois = gsap.timeline({
   chapitreTrois.to(".section03",{backgroundPosition:"100% 100%",duration:18,ease:'none'})
   chapitreTrois.to(".sec3-phrase01",{text:"Pour retourner chez elle, le duo devait parcourir plusieurs défis.", ease:'none', duration: 1.3}, '<+=3')
   chapitreTrois.to(".sec3-phrase01",{text:"Des crystaux, aussi tranchant qu'un couteau se dressaient devant elles.", ease:'none', duration: 1.3}, '<+=3')
-  chapitreTrois.to(".sec3-phrase01",{text:"Les évitants de près, le duo se fofilait pour les éviter.", ease:'none', duration: 1.3}, '<+=3')
+  chapitreTrois.to(".sec3-phrase01",{text:"Les évitants de près, le duo se faufilait pour les éviter.", ease:'none', duration: 1.3}, '<+=3')
   chapitreTrois.fromTo(".sec3-phrase01",{opacity: 1}, {opacity: 0, duration: 3}, '<3')
   chapitreTrois.fromTo(".crystal01", {x:'-115%'}, {x:'-5%', duration: 0.5 }, '5')
   chapitreTrois.to(".crystal01", {y:'-150%', duration: 7, ease: 'none' }, '<-0.5')
@@ -118,14 +118,49 @@ let chapitreTrois = gsap.timeline({
 
 
 // Section Quatre
-let chapitreQuatre = gsap.timeline();
+let chapitreQuatre = gsap.timeline() ;
+chapitreQuatre.to(".sec4-phrase02", {text:"Une grotte immense se dressa devant elles, ne laissant pas de choix autre que d'y entrer.", ease:'none', duration: 1.3}, '4')
+chapitreQuatre.to(".sec4-phrase02", {opacity: 0, duration: 1}, '<4')
 chapitreQuatre.to("#sec4-nuage", {x: '100vw', duration: 10, ease: 'power1.in'})
+chapitreQuatre.fromTo(".sec4-phrase01", {opacity: 0}, {opacity: 1, duration: 1}, '<4')
+chapitreQuatre.to(".sec4-phrase01", {opacity: 0, duration: 1}, '<6')
+chapitreQuatre.to("#section4-grotte", {x: '-225vw', duration: 3}, '-=2')
+chapitreQuatre.to(".grotte-groupe", {y:"-5%", yoyo: 'true', repeat: -1, ease: 'power1.inOut', duration: 0.5})
+chapitreQuatre.fromTo(".grotte-groupe", {opacity: 0}, {opacity: 1, duration: 2})
+chapitreQuatre.fromTo(".sec4-phrase04",{opacity: 0},{opacity: 1, duration: 1}, '<1')
+chapitreQuatre.to(".sec4-phrase04", {opacity: 0, duration: 2}, '<1')
+chapitreQuatre.to("#section04", {backgroundPosition:"100% 100%",duration:18,ease:'none'}, '<')
+chapitreQuatre.fromTo(".sec4-phrase03",{opacity: 0},{opacity: 1, duration: 1}, '<1')
+chapitreQuatre.to(".sec4-phrase03", {opacity: 0, duration: 2}, '<1')
+chapitreQuatre.to(".sec4-phrase04", {text: "T'as entendu ce bruit?", duration: 0},'<')
+chapitreQuatre.fromTo(".yeux-oiseau", {opacity: 0}, {opacity: 1, duration: 1},'<')
+chapitreQuatre.fromTo(".sec4-phrase05",{opacity: 0},{opacity: 1, duration: 1}, '<1')
+chapitreQuatre.to(".sec4-phrase05", {opacity: 0, duration: 2}, '<1')
+chapitreQuatre.fromTo(".sec4-phrase04",{opacity: 0},{opacity: 1, duration: 1}, '<1')
+chapitreQuatre.to(".sec4-phrase04", {opacity: 0, duration: 2}, '<1')
+chapitreQuatre.to(".sec4-phrase03", {text: "Ce n'était pas toi..", duration: 0},'<')
+chapitreQuatre.fromTo(".sec4-phrase03",{opacity: 0},{opacity: 1, duration: 1}, '<1')
+chapitreQuatre.to(".sec4-phrase03", {opacity: 0, duration: 2}, '<1')
+chapitreQuatre.to(".sec4-phrase04", {text: "Non...", duration: 0},'<')
+chapitreQuatre.fromTo(".sec4-phrase04",{opacity: 0},{opacity: 1, duration: 1}, '<1')
+chapitreQuatre.to(".sec4-phrase04", {opacity: 0, duration: 2}, '<1')
+chapitreQuatre.fromTo(".sec4-phrase05",{opacity: 0},{opacity: 1, duration: 1}, '<1')
+chapitreQuatre.to(".sec4-phrase05", {opacity: 0, duration: 2}, '<1')
+chapitreQuatre.to(".yeux-oiseau", {opacity: 0, duration: 1},'<')
+chapitreQuatre.to(".sec4-phrase04", {text: "AAAAAAAAAAA", duration: 0},'<1')
+chapitreQuatre.to(".sec4-phrase03", {text: "AAAAAAAAAAAAA", duration: 0},'<')
+chapitreQuatre.fromTo(".sec4-phrase04",{opacity: 0},{opacity: 1, duration: 1}, '<1')
+chapitreQuatre.fromTo(".sec4-phrase03",{opacity: 0},{opacity: 1, duration: 1}, '<')
+chapitreQuatre.to(".grotte-groupe, .sec4-phrase04, .sec4-phrase03", {x: '100vw', duration: 1},'<1')
+chapitreQuatre.to(".sec4-phrase04, .sec4-phrase03, .grotte-groupe",{opacity: 0, duration: 0});
+chapitreQuatre.to("#section4-grotte", {x: '-425vw', duration: 5},'<')
+chapitreQuatre.fromTo(".spritesheet1", {opacity:0} ,{opacity: 1},'<1')
 
-let chapitreQuatreGrotte = gsap.timeline();
-  chapitreQuatreGrotte.to(".grotte-groupe", {y:"-5%", yoyo: 'true', repeat: 20, ease: 'power1.inOut', duration: 0.5})
-  chapitreQuatreGrotte.fromTo(".yeux-oiseau", {opacity: 0}, {opacity: 1, duration: 0.5})
-  chapitreQuatreGrotte.to(".grotte-groupe", {x: '100vw', duration: 1, ease: 'power.in'})
+
+let chapitreCinq = gsap.timeline();
+  chapitreCinq.to(".sec5-phrase01", {text: "Il ne leur restait qu'un chemin à prendre, mais lequel..", duration: 2}, 3)
+  chapitreCinq.to(".sec5-phrase01", {text: '"Suivons cette enseigne", cria Lilo', duration: 2}, '<5')
+
 
 // Section Six
 let chapitreSix = gsap.timeline();
-chapitreSix.fromTo('.escalier', {x: '100%'}, {x: '-10px', duration: 1, stagger: 0.1})
